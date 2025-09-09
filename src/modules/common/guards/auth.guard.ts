327082c4c8185017c9310b1f0f1cc1a15b5101af
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-09-09 12:55:30 +0200

    Add auth guard

---
+++ src/modules/common/guards/auth.guard.ts
@@ -0,0 +1,40 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+interface JwtPayload {
+  id: string;
+  isAdmin: boolean;
+}
+
+/**
+ * Check is User is connected
+ */
+@Injectable()
+export class IsAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = request.headers.authorization;
+
+    if (!token) {
+      throw new UnauthorizedException(
+        'Missing or invalid Authorization header',
+      );
+    }
+
+    try {
+      request['user'] = this.jwtService.verify<JwtPayload>(token);
+      return true;
+    } catch (error) {
+      console.error(error);
+      throw new UnauthorizedException('Invalid or expired token');
+    }
+  }
+}
