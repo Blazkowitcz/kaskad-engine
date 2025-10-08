d28e709ff100968e017af5188605e3d7390bcc1e
Blazkowicz <bonnet.nicolas66@gmail.com>
2025-10-08 22:33:22 +0200

    DRAFT : Add CORS and Origin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:4000',
+      'http://127.0.0.1:4000',
+      'http://192.168.1.86:4000',
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
